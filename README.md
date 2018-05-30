eb7d91eb2e6fd5add82e286dbe08be2cd88c71fa
Nikita Platonov <platonov@fishlab.su>
2018-05-30 14:13:59 +0300

    plugin

---
+++ README.md
@@ -0,0 +1,45 @@
+# Gulp project map
+[![NPM version](https://img.shields.io/badge/npm-1.1.1-brightgreen.svg)](https://www.npmjs.com/package/gulp-project-map)
+[![devDependency Status](https://david-dm.org/gladkih/gulp-project-map/dev-status.svg)](https://david-dm.org/gladkih/gulp-project-map#info=devDependencies)
+
+> Generate list of file for the project (html sitemap)
+
+## Install
+Install with [npm](https://npmjs.org/package/gulp-project-map)
+
+```bash
+$ npm install --save-dev gulp-project-map
+```
+
+## Example
+
+```js
+var gulp = require('gulp');
+var projectMap = require('gulp-project-map');
+
+gulp.task('projectMap', function () {
+    projectMap({
+        path: './build'
+      });
+});
+```
+
+### Options
+Type: `Object | Null`<br/>Default:
+
+```js
+{
+  path: './',
+  extension: '.html',
+  name: 'result',
+  title: 'project-map'
+}
+```
+
+- `path` (String) Directory that needs file list created
+- `extension` (String) File extension<br/>
+- `name` (String) Name of file where list is created<br/>
+- `title` (String) File header<br/>
+
+## License
+MIT ©[Maxim Gladkih](https://gladkih.su)
---
+++ index.js
@@ -0,0 +1,136 @@
+'use strict';
+
+var fs = require('fs');
+var extend = require('extend');
+var jade = require('jade');
+var path = require('path');
+var cheerio = require('cheerio');
+
+function getFiles(options) {
+    options = extend({
+        path: './',
+        extension: '.html',
+        name: 'result',
+        title: 'project-map'
+    }, options);
+
+    walker(options.path, options.extension, function(error, result) {
+
+        if (error) {
+            throw error;
+        }
+
+        var data = {
+            title: options.title,
+            list: []
+        };
+        var len = result.length;
+
+        Promise.prototype.thenReturn = function(value) {
+            return this.then(function() {
+                return value;
+            });
+        };
+
+        function readFile(filePath) {
+            return new Promise(function(resolve) {
+                getTitle(filePath, options.path, function(title) {
+                    data.list.push(title);
+                    resolve();
+                });
+            });
+        }
+
+        Promise.resolve(0).then(function loop(i) {
+            if (i < len) {
+                return readFile(result[i])
+                    .thenReturn(i + 1)
+                    .then(loop);
+            }
+        }).then(function() {
+            writeResultFile(options, data);
+            console.log('done');
+        }).catch(function(error) {
+            console.log('error', error);
+        });
+
+    });
+}
+
+/**
+ * Write result file
+ * @param options
+ * @param data
+ */
+function writeResultFile(options, data) {
+    jade.renderFile(__dirname + '/result.jade', {pretty: true, data: data}, function(error, html) {
+        if (error) {
+            console.log('error:\n' + error);
+        }
+
+        fs.writeFile(options.path + '/' + options.name + '.html', html, function(err) {
+            if (err) {
+                return console.log(err);
+            }
+        });
+    });
+}
+
+function walker(directory, extension, callback) {
+    var results = [];
+    fs.readdir(directory, function(error, list) {
+        if (error) {
+            return callback(error);
+        }
+        var i = 0;
+        (function next() {
+            var file = list[i];
+            i += 1;
+            if (!file) {
+                return callback(null, results);
+            }
+            file = directory + '/' + file;
+            fs.stat(file, function(error, stat) {
+                if (stat && stat.isDirectory()) {
+                    walker(file, extension, function(error, res) {
+                        results = results.concat(res);
+                        next();
+                    });
+                } else {
+                    if (path.extname(file) === extension) {
+                        results.push(file);
+                    }
+                    next();
+                }
+            });
+        })();
+    });
+}
+
+/**
+ *
+ * @param url
+ * @param removePath
+ * @param callback
+ */
+function getTitle(url, removePath, callback) {
+    callback = callback ? callback : function() {
+    };
+    fs.readFile(url, 'utf8', function(error, data) {
+        url = url.replace(removePath + '/', '');
+        var _fileInfo = {
+            title: url,
+            link: url
+        };
+        if (error) {
+            callback(_fileInfo);
+        }
+        var $ = cheerio.load(data);
+        var _title = $('h1');
+
+        _fileInfo.title = _title.text() || url;
+        callback(_fileInfo);
+    });
+}
+
+module.exports = getFiles;
